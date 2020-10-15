a2722a821698b3a236d2064111deb9be5d1bf31a
Eduardo Souza <lkledu@gmail.com>
2020-10-15 20:06:24 -0300

    first commit

---
+++ src/main/java/com/ciet/Skyline/SkylineApplication.java
@@ -0,0 +1,13 @@
+package com.ciet.Skyline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylineApplication.class, args);
+	}
+
+}
