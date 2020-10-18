9c4d4197490fdc552078a8d474f3fbf953382907
Eduardo Souza <lkledu@gmail.com>
2020-10-18 02:08:11 -0200

    criando método para adicionar saldo

--- src/main/java/com/ciet/Skyline/model/SkylineModel.java
+++ src/main/java/com/ciet/Skyline/model/SkylineModel.java
@@ -10,22 +10,25 @@ public class SkylineModel {
     //private ConexaoDB conn = new ConexaoDB("localhost", "5432", "SkylineDB", "skyline_user", "12345");
     private ConexaoDB conn = new ConexaoDB();
     
-    private boolean cpfNotExist(ConexaoDB conn, String cpf){
+    private boolean cpfExist(ConexaoDB conn, String cpf){
         try {
             String query = "SELECT COUNT(*) FROM \"public\".\"Clientes\" WHERE cpf ='"+ cpf +"';";
             ResultSet result = this.conn.query(query);
             while(result.next()){
                 return true;
             }
-        } catch (SQLException e) { e.printStackTrace(); }
-        return false;
+            return false;
+        } catch (SQLException e) { 
+            e.printStackTrace();
+            return false;
+        }
     }
     
     public boolean cadastroCliente(String nome, String cpf){
         boolean response = false;
         
         this.conn.connect();
-        if(this.cpfNotExist(this.conn, cpf)){
+        if(!this.cpfExist(this.conn, cpf)){
             String query = "INSERT INTO \"public\".\"Clientes\"(nome, cpf, saldo) VALUES('"+ nome +"','"+ cpf +"','0.0');";
             this.conn.update(query);
             response = true;
@@ -37,7 +40,7 @@ public class SkylineModel {
     public void creditarSaldo(String cpf, double saldo){
         conn.connect();
 
-
+        String query = "UPDATE INTO \"public\".\"Clientes\" WHERE cpf='"+cpf+"';";
 
         conn.disconnect();
     }
